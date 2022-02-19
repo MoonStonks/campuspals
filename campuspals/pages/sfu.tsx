17a079ed788e44211b2cc54c12e198f1ce5f3451
StevenZhao318 <stevenzhao318@hotmail.com>
2022-02-19 11:59:57 -0800

    add layout
    Co-Authored-By: Ben Cheung <MrBenC88@users.noreply.github.com>
    Co-Authored-By: Scott <46171819+scott-m-king@users.noreply.github.com>

--- campuspals/pages/sfu.tsx
+++ campuspals/pages/sfu.tsx
@@ -1,17 +1,25 @@
 import { Button, Flex, Heading, VStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
+import Layout from '../components/Layout';
 
 const SFUPage = () => (
-  <Flex h='100vh' alignItems='center' justifyContent='center' flexDir='column'>
-    <Heading>Hello</Heading>
-    <Flex direction='column' background='gray.100' p={10} rounded={6}>
-      <Heading mb={2}>Simon Fraser University</Heading>
-      <NextLink href='/universities'>
-        <Button colorScheme='teal'>Go Back</Button>
-      </NextLink>
+  <Layout>
+    <Flex
+      h='calc(100vh - 49px)'
+      alignItems='center'
+      flexDir='column'
+      bg='red.100'
+    >
+      <Heading>Hello</Heading>
+      <Flex direction='column' background='gray.100' p={10} rounded={6}>
+        <Heading mb={2}>Simon Fraser University</Heading>
+        <NextLink href='/universities'>
+          <Button colorScheme='teal'>Go Back</Button>
+        </NextLink>
+      </Flex>
     </Flex>
-  </Flex>
+  </Layout>
 );
 
 export default SFUPage;
--- campuspals/pages/ubc.tsx
+++ campuspals/pages/ubc.tsx
@@ -1,16 +1,19 @@
 import { Button, Flex, Heading } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
+import Layout from '../components/Layout';
 
 const UBCPage = () => (
-  <Flex h='100vh' alignItems='center' justifyContent='center'>
-    <Flex direction='column' background='gray.100' p={10} rounded={6}>
-      <Heading mb={2}>University of British Columbia</Heading>
-      <NextLink href='/universities'>
-        <Button colorScheme='teal'>Go Back</Button>
-      </NextLink>
+  <Layout>
+    <Flex h='calc(100vh - 49px)' alignItems='center' bg='blue.100'>
+      <Flex direction='column' background='gray.100' p={10} rounded={6}>
+        <Heading mb={2}>University of British Columbia</Heading>
+        <NextLink href='/universities'>
+          <Button colorScheme='teal'>Go Back</Button>
+        </NextLink>
+      </Flex>
     </Flex>
-  </Flex>
+  </Layout>
 );
 
 export default UBCPage;
